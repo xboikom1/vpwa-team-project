dedc1c48fbca588e62131add5531ed387aabfd7f
Boiko_Maksym <xboikom1@stuba.sk>
2026-09-17 14:34:50 +0200

    init project

---
+++ frontend/postcss.config.js
@@ -0,0 +1,21 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+import autoprefixer from 'autoprefixer';
+// import rtlcss from 'postcss-rtlcss'
+// import { Mode } from 'postcss-rtlcss/options'
+
+export default {
+  plugins: [
+    // https://github.com/postcss/autoprefixer
+    autoprefixer({
+      overrideBrowserslist: ['baseline widely available'],
+    }),
+
+    // https://github.com/elchininet/postcss-rtlcss
+    // If you want to support RTL css, then
+    // 1. yarn/pnpm/bun/npm install postcss-rtlcss
+    // 2. optionally set quasar.config.js > framework > lang to an RTL language
+    // 3. uncomment the following line (and its import statement above):
+    // rtlcss({ mode: Mode.Override })
+  ],
+};
